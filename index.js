ab5701989cfb7ef9783e4a5d657af1cda455a474
timoharju <timoharju17@gmail.com>
2020-09-22 12:47:10 +0300

    test backend build for mongodb

---
+++ index.js
@@ -0,0 +1,12 @@
+var MongoClient = require('mongodb').MongoClient;
+var dotenv = require('dotenv');
+
+dotenv.config();
+
+var url = process.env.MONGODB_URI;
+
+MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    console.log("Database created!");
+    db.close();
+  });
