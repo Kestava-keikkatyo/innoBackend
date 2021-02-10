a08e7ca76c4cdb7cb3c902eab8c424671e54872a
Joel <joel.lainebest@gmail.com>
2021-02-10 23:37:16 +0200

    Feelings post-kutsu toimii nyt

--- controllers/feelings.js
+++ controllers/feelings.js
@@ -9,21 +9,30 @@ const Business = require("../models/Business")
 const { needsToBeWorker, needsToBeAgency, needsToBeBusiness } = require("../utils/middleware")
 
 /**
+ * Returns response.body: { The updated Worker object }
  * Route for user to add a feeling.
  * request.body requirements: {value: Int}. That is the minimum, can also be {value: Int, note: "note"}
  * Must be logged in as user
  */
 feelingsRouter.post("/", authenticateToken, needsToBeWorker, async (request, response, next) => {
-  const body = request.body
-  if (body.value) {
-
-  }
-
-  if (body.note) {
-
-  }
-
   try {
+    const body = request.body
+    if (body.value !== undefined) {
+      User.findByIdAndUpdate(
+        // User id got from middleware.js. AddToSet adds 'value' and 'note' to feelings array. Note not added if undefined.
+        response.locals.decoded.id,
+        { $addToSet: { feelings: [{ value: body.value, note: body.note }] } },
+        { new: true, omitUndefined: true, runValidators: true },
+        (error, result) => {
+          if (!result || error) {
+            return response.status(401).send(error || { message: "Not authorized" })
+          } else {
+            return response.status(200).send(result)
+          }
+        })
+    } else {
+      return response.status(400).send({ error: "Request body must include 'value' field" })
+    }
 
   } catch (exception) {
     next(exception)
