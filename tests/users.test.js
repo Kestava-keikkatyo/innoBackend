c922f8c08078dfcfb29cfc4b8a04a94c3b965969
Janne <janne13_78@hotmail.com>
2021-03-17 04:59:53 +0200

    Updated tests

--- tests/users.test.js
+++ tests/users.test.js
@@ -1,25 +1,30 @@
-const User = require("../models/User")
-const supertest = require("supertest")
-const mongoose = require("mongoose")
-const helper = require("./test_helper")
-const app = require("../app")
+import User, { deleteMany } from "../models/User"
+import supertest from "supertest"
+import { connection } from "mongoose"
+import { usersInDb } from "./test_helper"
+import app from "../app"
 const api = supertest(app)
 
 describe("when there is initially one user at db", () => {
   beforeEach(async () => {
-    await User.deleteMany({})
-    const user = new User({ name: "root", email: "test@adad.com", password: "sekret" })
+    await deleteMany({})
+    // const user = new User({ name: "root", email: "test@adad.com", password: "sekret" })
+    const hash = await bcrypt.hash("salis", 10)
+    const user = new User({ name: "testiukkeli", email: "testiukkeli@testi.org", passwordHash: hash })
     await user.save()
   })
 
   test("creation succeeds with a fresh name", async () => {
-    const usersAtStart = await helper.usersInDb()
+    const usersAtStart = await usersInDb()
 
-    const newUser = {
-      name: "testi",
-      email: "test@test.com",
-      password: "salasana",
-    }
+    // const newUser = {
+    //   name: "testi",
+    //   email: "test@test.com",
+    //   password: "salasana",
+    // }
+
+    const hash = await bcrypt.hash("salis", 10)
+    const newUser = new User({ name: "testiukkeli2", email: "testiukkeli2@testi.org", passwordHash: hash })
 
     await api
       .post("/api/users")
@@ -27,7 +32,7 @@ describe("when there is initially one user at db", () => {
       .expect(200)
       .expect("Content-Type", /application\/json/)
 
-    const usersAtEnd = await helper.usersInDb()
+    const usersAtEnd = await usersInDb()
     expect(usersAtEnd.length).toBe(usersAtStart.length + 1)
 
     const usernames = usersAtEnd.map(u => u.name)
@@ -35,5 +40,5 @@ describe("when there is initially one user at db", () => {
   })
 })
 afterAll(() => {
-  mongoose.connection.close()
+  connection.close()
 })
