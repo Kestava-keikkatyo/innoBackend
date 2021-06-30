81c25967bbe47ffff1cd8fc833d509eb6484d2e5
AbdullahHinnawi <hinnawiabdullah@gmail.com>
2021-06-30 19:25:24 +0300

    index.ts updated

--- index.ts
+++ index.ts
@@ -5,6 +5,6 @@ import config from "./utils/config"
 const server = http.createServer(app)
 
 server.listen(config.PORT, () => {
-  console.log(`Server running at ${config.IP}:${config.PORT}`)
-  console.log(`API docs available at ${config.IP}:${config.PORT}/api-docs`)
+  console.log(`Server running at http://${config.IP}:${config.PORT}`)
+  console.log(`API docs available at http://${config.IP}:${config.PORT}/api-docs`)
 })
