218155a07bd9adf9cd5cfe0384d3336b84b82e3a
Walter R <waltsu123123@gmail.com>
2021-07-01 13:05:46 +0300

    profile controllers muutoksia

--- controllers/profile.ts
+++ controllers/profile.ts
@@ -3,8 +3,8 @@ import { IProfileDocument } from "../objecttypes/modelTypes"
 import authenticateToken from "../utils/auhenticateToken"
 import { needsToBeAgencyBusinessOrWorker } from "../utils/middleware"
 import Profile from "../models/Profile"
-import { IBodyWithProfile } from "../objecttypes/otherTypes"
-import {CallbackError, DocumentDefinition} from "mongoose"
+import { IBaseBody, IBodyWithProfile } from "../objecttypes/otherTypes"
+import { CallbackError, DocumentDefinition } from "mongoose"
 import {IAgencyDocument, IBusinessDocument, IWorkerDocument} from "../objecttypes/modelTypes"
 
 import { error as _error, info as _info } from "../utils/logger"
@@ -12,6 +12,8 @@ import { error as _error, info as _info } from "../utils/logger"
 import Business from "../models/Business"
 import Agency from "../models/Agency"
 import Worker from "../models/Worker"
+import { ParamsDictionary } from "express-serve-static-core"
+
 
 
 
@@ -130,6 +132,58 @@ profileRouter.get('/:id', authenticateToken, needsToBeAgencyBusinessOrWorker, (r
 })
 
 
+profileRouter.put("/:profileId", authenticateToken, async (req: Request<ParamsDictionary, unknown, IBaseBody>, res: Response, next: NextFunction) => {
+
+
+  try {
+    updateProfile(req,res,next)
+  } catch (exception) {
+    return next(exception)
+  }
+})
+
+/**
+ * Helper function for updating the form. Helps reduce duplicate code.
+ * @param agencyOrBusinessOrWorkerObject request.agency or request.business or worker. Depending on which one is trying to update their profile
+ * @param req Request
+ * @param res Response
+ * @param next NextFunction
+ */
+const updateProfile = (req: Request<ParamsDictionary, unknown, IBaseBody>, res: Response, next: NextFunction) => {
+  const { params } = req
+  try {
+    const profileId: string = params.profileId
+    let found: boolean = false
+    /*
+    if (agencyOrBusinessOrWorkerObject.profile.length === 0) {
+      return res.status(403).send({ message: "You are not authorized to update this form" })
+    }
+    */
+    
+     
+       
+        Profile.findByIdAndUpdate(
+          profileId,
+          { ...req.body }, // Give the full form object, or the full questions object in said object, in body when updating questions. Otherwise all other questions are deleted.
+          { new: true, runValidators: true, lean: true },
+          (error: CallbackError, result: DocumentDefinition<IProfileDocument> | null) => {
+            if (error || !result) {
+              return res.status(500).send(error || { message: "Didn't get a result from database while updating profile" })
+            } else {
+              found = true;
+              return res.status(200).send(result)
+            }
+          }
+        )
+      
+    if (!found) {
+      return res.status(404).send({ message: `Could not find profile with id ${profileId}` })
+    }
+  } catch (exception) {
+    return next(exception)
+  }
+}
+/*
 profileRouter.put('/:id', (request, response, next) => {
   const body = request.body
 
@@ -148,6 +202,6 @@ profileRouter.put('/:id', (request, response, next) => {
     })
     .catch(error => next(error))
 })
-
+*/
   export default profileRouter
 
