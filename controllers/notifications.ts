d7d203c686aece863cc43cedb9db3bdab9c236c5
Vertti Ahlstén <vertti.ahlsten@gmail.com>
2022-02-12 12:34:30 +0200

    Changed notifications to use new authentication.

--- controllers/notifications.ts
+++ controllers/notifications.ts
@@ -1,29 +1,29 @@
 import express from 'express'
 import authenticateToken from '../utils/auhenticateToken'
-import { needsToBeAgencyBusinessOrWorker } from '../utils/middleware'
+import { isWorkerOrBusinessOrAgency } from '../utils/authJwt'
 import { postNotificationDocument, sendTextToNotificationsDocument, getNotificationsDocument, textReadNotificationsDocument, clearAllNotificationsDocument } from '../utils/notificationsMiddleware'
 
 const notificationsRouter = express.Router()
 /**
  * Käytetään kun käyttäjä ensimmäisen kerran rekistöröityy sovellukseen.
  */
-notificationsRouter.post("/post",authenticateToken,needsToBeAgencyBusinessOrWorker,postNotificationDocument)
+notificationsRouter.post("/post",authenticateToken,isWorkerOrBusinessOrAgency,postNotificationDocument)
 /**
  * Käytetään kun käyttäjälle halutaan tehdä ilmoitus.
  */
-notificationsRouter.put("/:userId/update",authenticateToken,needsToBeAgencyBusinessOrWorker,sendTextToNotificationsDocument)
+notificationsRouter.put("/:userId/update",authenticateToken,isWorkerOrBusinessOrAgency,sendTextToNotificationsDocument)
 /**
- * Käytetään kun halutaan hakea käyttäjän ilmoitukset, haun jälkeen ilmoitukset näytetään 
+ * Käytetään kun halutaan hakea käyttäjän ilmoitukset, haun jälkeen ilmoitukset näytetään
  * kelloiconissa käyttöliittymältä.
  */
-notificationsRouter.get("/get",authenticateToken,needsToBeAgencyBusinessOrWorker,getNotificationsDocument)
+notificationsRouter.get("/get",authenticateToken,isWorkerOrBusinessOrAgency,getNotificationsDocument)
 /**
  * Käytetään kun käyttäjä näkee ilmoituksen ja klikkaa sitä. Ilmoitus merkataan tämän jälkeen luetuksi.
  */
-notificationsRouter.put("/:textId/read",authenticateToken,needsToBeAgencyBusinessOrWorker,textReadNotificationsDocument)
+notificationsRouter.put("/:textId/read",authenticateToken,isWorkerOrBusinessOrAgency,textReadNotificationsDocument)
 /**
  * Käytetään kun käyttäjä painaa kaikki ilmoitukset luetuiksi.
  */
-notificationsRouter.put("/clearAll",authenticateToken,needsToBeAgencyBusinessOrWorker,clearAllNotificationsDocument)
+notificationsRouter.put("/clearAll",authenticateToken,isWorkerOrBusinessOrAgency,clearAllNotificationsDocument)
 
 export default notificationsRouter
