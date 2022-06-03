28776e0129eccc223a0dc2445aaa3ac55a051d41
Amal Kayed <amal.kayed88@gmail.com>
2022-06-03 12:14:51 +0300

    Added new function to get user's work requests

--- middleware/workRequestMiddleware.ts
+++ middleware/workRequestMiddleware.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import WorkRequest from "../models/WorkRequest";
 import { IWorkRequestDocument } from "../objecttypes/modelTypes";
+import { CallbackError } from "mongoose";
 import { copyProperties } from "../utils/common";
 
 const updatableFields = [
@@ -38,6 +39,38 @@ export const postWorkRequest = async (
         .send({ error: "Failed to create a work request!" });
     }
     return res.status(200).send(workRequest);
+  } catch (exception) {
+    return next(exception);
+  }
+};
+
+/**
+ * Get user's work requests.
+ * @param {Request} req - Express Request.
+ * @param {Response} res - Express Response.
+ * @param {NextFunction} next
+ * @returns user's work requests
+ */
+export const getMyWorkRequests = (
+  _req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    WorkRequest.find(
+      { user: res.locals.userId },
+      (error: CallbackError, docs: IWorkRequestDocument[]) => {
+        if (error) {
+          return res.status(500).json({ message: error.message });
+        }
+        if (!docs.length) {
+          return res.status(404).json({ message: "No work requests found!" });
+        }
+        return res.status(200).json(docs);
+      }
+    ).populate("user", {
+      name: 1,
+    });
   } catch (exception) {
     return next(exception);
   }
