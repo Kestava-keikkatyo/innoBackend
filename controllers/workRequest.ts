3d4ba5fc04b9c5d1f3b8c9ab23c02b2976541b4b
Amal Kayed <amal.kayed88@gmail.com>
2022-06-05 10:08:44 +0300

    Endpoint for user of role business to update a work request

--- controllers/workRequest.ts
+++ controllers/workRequest.ts
@@ -5,6 +5,7 @@ import {
   getMyWorkRequests,
   getWorkRequestById,
   postWorkRequest,
+  updateWorkRequest,
 } from "../middleware/workRequestMiddleware";
 
 const workRequestRouter = express.Router();
@@ -131,6 +132,57 @@ workRequestRouter.get(
   tokenAuthentication,
   isBusiness,
   getWorkRequestById
+);
+
+/**
+ * Route for user of role business to update own work request.
+ * @openapi
+ * /workRequest/update/{id}:
+ *   put:
+ *     summary: Route for user of role business to update own work request.
+ *     description: Must be logged in as a business.
+ *     tags: [WorkRequest, Business]
+ *     parameters:
+ *       - in: header
+ *         name: x-access-token
+ *         description: The token you get when logging in is used here. Used to authenticate the user.
+ *         required: true
+ *         schema:
+ *           $ref: "#/components/schemas/AccessToken"
+ *       - in: path
+ *         name: id
+ *         description: ID of the work request to be updated.
+ *         required: true
+ *         schema:
+ *           type: string
+ *           example: 604021e581a9626810885657
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: "#/components/schemas/WorkRequest"
+ *     responses:
+ *       "200":
+ *         description: Returns the updated work request.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: "#/components/schemas/WorkRequest"
+ *       "404":
+ *         description: No work request was found with the requested ID.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: "#/components/schemas/Error"
+ *             example:
+ *               message: This work request is not existing
+ */
+workRequestRouter.put(
+  "/update/:id",
+  tokenAuthentication,
+  isBusiness,
+  updateWorkRequest
 );
 
 export default workRequestRouter;
