8ec488df009db15337ccf6a8714ea43179cf5be1
Amal Kayed <amal.kayed88@gmail.com>
2022-06-27 16:00:36 +0300

    Updated job endpoint, swagger comments and deleted unused code

--- controllers/job.ts
+++ controllers/job.ts
@@ -9,7 +9,7 @@ import {
   getJobAds,
   getMyJobById,
 } from "../middleware/jobMiddleware";
-import { isAdmin, isAgency, isWorker, isWorkerOrBusinessOrAgency } from "../utils/authJwt";
+import { isAgency, isWorkerOrAdmin } from "../utils/authJwt";
 import { tokenAuthentication } from "../middleware/authenticationMiddleware";
 const jobRouter = express.Router();
 
@@ -53,13 +53,13 @@ const jobRouter = express.Router();
 jobRouter.post("/", tokenAuthentication, isAgency, postJob);
 
 /**
- * Route for users of role worker to get all available job ads.
+ * Route for users of role worker or admin to get all available job ads.
  * @openapi
  * /job/ads:
  *   get:
- *     summary: Route for users or role worker to get all available job ads
- *     description: Need to be logged in as a user of role worker.
- *     tags: [Job, Worker]
+ *     summary: Route for users or role worker or admin to get all available job ads
+ *     description: Need to be logged in as a user of role worker or admin.
+ *     tags: [Job, Worker, Admin]
  *     parameters:
  *       - in: header
  *         name: x-access-token
@@ -85,7 +85,7 @@ jobRouter.post("/", tokenAuthentication, isAgency, postJob);
  *             example:
  *               message: No job ads found
  */
-jobRouter.get("/ads", tokenAuthentication, isWorker, getJobAds);
+jobRouter.get("/ads", tokenAuthentication, isWorkerOrAdmin, getJobAds);
 
 /**
  * Route for user of role agency to get own created job by its id
@@ -132,9 +132,9 @@ jobRouter.get("/my/any/:id", tokenAuthentication, isAgency, getMyJobById);
  * @openapi
  * /job/any/{id}:
  *   get:
- *     summary: Route for worker or agency to get a job by its id
- *     description: Must be logged in as worker or agency.
- *     tags: [Jobs, Worker, Agency]
+ *     summary: Route for user of role worker or admin to get a job by its id
+ *     description: Must be logged in as user of role worker or admin.
+ *     tags: [Job, Worker, Admin]
  *     parameters:
  *       - in: header
  *         name: x-access-token
@@ -165,7 +165,7 @@ jobRouter.get("/my/any/:id", tokenAuthentication, isAgency, getMyJobById);
  *             example:
  *               message:The requested job is not existing
  */
-jobRouter.get("/any/:id", tokenAuthentication, isWorkerOrBusinessOrAgency, getJobById);
+jobRouter.get("/any/:id", tokenAuthentication, isWorkerOrAdmin, getJobById);
 
 /**
  * Route for agencies to get their own jobs
@@ -201,87 +201,6 @@ jobRouter.get("/any/:id", tokenAuthentication, isWorkerOrBusinessOrAgency, getJo
  *               message: No jobs found
  */
 jobRouter.get("/my", tokenAuthentication, isAgency, getMyJobs);
-
-/**
- * Route for admin to get all jobs.
- * @openapi
- * /job/allJobsForAdmin:
- *   get:
- *     summary: Route for admin to get all jobs
- *     description: Need to be logged in as a admin.
- *     tags: [Jobs, Admin]
- *     parameters:
- *       - in: header
- *         name: x-access-token
- *         description: The token you get when logging in is used here. Used to authenticate the user.
- *         required: true
- *         schema:
- *           $ref: "#/components/schemas/AccessToken"
- *     responses:
- *       "200":
- *         description: Returns found jobs
- *         content:
- *           application/json:
- *             schema:
- *               type: array
- *               items:
- *                 $ref: "#/components/schemas/Job"
- *       "404":
- *         description: No jobs are existing
- *         content:
- *           application/json:
- *             schema:
- *               $ref: "#/components/schemas/Error"
- *             example:
- *               message: No jobs found
- */
-jobRouter.get("/allJobsForAdmin", tokenAuthentication, isAdmin, getJobAds);
-
-/**
- * Route for admin to get job by its id
- * @openapi
- * /job/jobForAdmin/{id}:
- *   get:
- *     summary: Route for admin to get job by its id
- *     description: Must be logged in as admin.
- *     tags: [Job, Admin]
- *     parameters:
- *       - in: header
- *         name: x-access-token
- *         description: The token you get when logging in is used here. Used to authenticate the user.
- *         required: true
- *         schema:
- *           $ref: "#/components/schemas/AccessToken"
- *       - in: path
- *         name: id
- *         description: ID of the requested job.
- *         required: true
- *         schema:
- *           type: string
- *           example: 604021e581a9626810885657
- *     responses:
- *       "200":
- *         description: Returns the requested job.
- *         content:
- *           application/json:
- *             schema:
- *               $ref: "#/components/schemas/Job"
- *       "404":
- *         description: No job was found with the requested ID.
- *         content:
- *           application/json:
- *             schema:
- *               $ref: "#/components/schemas/Error"
- *             example:
- *               message:No job with ID {id} found
- *       "500":
- *         description: An error occurred when calling the database.
- *         content:
- *           application/json:
- *             schema:
- *               $ref: "#/components/schemas/Error"
- */
-jobRouter.get("/jobForAdmin/:id", tokenAuthentication, isAdmin, getJobById);
 
 /**
  * Route for agency to update own job.
