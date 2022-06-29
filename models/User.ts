bf99ec73594770451a02c1d87ac9925a4d7d11be
Amal Kayed <amal.kayed88@gmail.com>
2022-06-29 13:07:41 +0300

    Refactoring feelings in user Model

--- models/User.ts
+++ models/User.ts
@@ -124,22 +124,58 @@ const userSchema: Schema = new Schema({
   ],
   feelings: [
     {
-      value: {
+      comfortable: {
         type: Number,
-        required: true,
-        min: [0, "Feelings can't go below 0"],
-        max: [4, "Feelings can't be above 4"],
+        required: false,
+        min: [0, "Comfortable can't go below 0"],
+        max: [4, "Comfortable can't be above 4"],
       },
-      note: {
+      satisfied: {
+        type: Number,
+        required: false,
+        min: [0, "Satisfied can't go below 0"],
+        max: [4, "Satisfied can't be above 4"],
+      },
+      energetic: {
+        type: Number,
+        required: false,
+        min: [0, "Energetic can't go below 0"],
+        max: [4, "Energetic can't be above 4"],
+      },
+      enthusiastic: {
+        type: Number,
+        required: false,
+        min: [0, "Enthusiastic can't go below 0"],
+        max: [4, "Enthusiastic can't be above 4"],
+      },
+      frustrated: {
+        type: Number,
+        required: false,
+        min: [0, "Frustrated can't go below 0"],
+        max: [4, "Frustrated can't be above 4"],
+      },
+      stressed: {
+        type: Number,
+        required: false,
+        min: [0, "Stressed can't go below 0"],
+        max: [4, "Stressed can't be above 4"],
+      },
+      anxious: {
+        type: Number,
+        required: false,
+        min: [0, "Anxious can't go below 0"],
+        max: [4, "Anxious can't be above 4"],
+      },
+      comment: {
+        type: String,
+      },
+      fileUrl: {
         type: String,
       },
       createdAt: {
         type: Date,
         default: Date.now,
         immutable: true,
-      },
-      fileUrl: {
-        type: String,
       },
     },
   ],
